76a451fd42545895025d30c48cca7e1a5b74a455
inigodm <inigo.delgado@gmail.com>
2020-07-26 23:02:35 +0200

    Add first tests

--- src/main/kotlin/inigo/objectMotherCreator/ObjectCreateAction.kt
+++ src/main/kotlin/inigo/objectMotherCreator/ObjectCreateAction.kt
@@ -1,8 +1,5 @@
 package inigo.objectMotherCreator
 
-import com.intellij.ide.util.TreeFileChooser
-import com.intellij.ide.util.TreeFileChooserDialog
-import com.intellij.ide.util.TreeFileChooserFactory
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
@@ -10,15 +7,25 @@ import com.intellij.openapi.actionSystem.PlatformDataKeys
 import com.intellij.openapi.editor.CaretModel
 import com.intellij.openapi.fileChooser.FileChooser
 import com.intellij.openapi.fileChooser.FileChooserDescriptor
-import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.NlsContexts
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiJavaFile
-import org.jetbrains.annotations.NotNull
-import org.jetbrains.annotations.Nullable
 
-class ObjectCreateAction : AnAction() {
+abstract class ObjectCreateAction : AnAction() {
+    fun createObjectMother(project: Project, e: AnActionEvent) {
+        val creator = ObjectMotherGenerator(e.getData(CommonDataKeys.PSI_FILE) as PsiJavaFile)
+        var descriptor = FileChooserDescriptor(false, true, false, false, false, false)
+        descriptor.title = "Choose project/module's test sourceDir in which ObjectMother's package will exist"
+        descriptor.description = "The source directory for ObjectMother's package must be set. Package name is automatically calculated"
+        FileChooser.chooseFile(
+            descriptor,
+            project,
+            e.getData(CommonDataKeys.VIRTUAL_FILE),
+            { creator.generateObjectMother(project, it) })
+    }
+}
+
+class ObjectCreateOnCaretSelectedAction : ObjectCreateAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
         val caretModel = e.getRequiredData(CommonDataKeys.EDITOR).caretModel
@@ -36,7 +43,7 @@ class ObjectCreateAction : AnAction() {
     }
 }
 
-class ObjectCreateFileSeletedAction : AnAction() {
+class ObjectCreateFileSeletedAction : ObjectCreateAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
         val selectedFile = e.getData(PlatformDataKeys.VIRTUAL_FILE)
@@ -54,19 +61,4 @@ class ObjectCreateFileSeletedAction : AnAction() {
 
     private fun isAnyJavaFileSelected(selectedFile: VirtualFile?) =
         selectedFile != null && selectedFile.toString().endsWith(".java")
-}
-
-private fun createObjectMother(
-    project: Project,
-    e: AnActionEvent
-) {
-    val creator = ObjectMotherGenerator(e.getData(CommonDataKeys.PSI_FILE) as PsiJavaFile)
-    var descriptor = FileChooserDescriptor(false, true, false, false, false, false)
-    descriptor.title = "Choose project/module's test sourceDir in which ObjectMother's package will exist"
-    descriptor.description = "The source directory for ObjectMother's package must be set. Package name is automatically calculated"
-    FileChooser.chooseFile(
-        descriptor,
-        project,
-        e.getData(CommonDataKeys.VIRTUAL_FILE),
-        { creator.generateObjectMother(project, it) })
 }
---
+++ src/test/kotlin/IntellijAction.kt
@@ -0,0 +1,57 @@
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.actionSystem.Presentation
+import com.intellij.openapi.editor.CaretModel
+import com.intellij.openapi.vfs.VirtualFile
+import inigo.objectMotherCreator.ObjectCreateFileSeletedAction
+import inigo.objectMotherCreator.ObjectCreateOnCaretSelectedAction
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.spyk
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class `Option in menu appears` {
+    @MockK
+    lateinit var actionEvent : AnActionEvent
+    @MockK
+    lateinit var caret : CaretModel
+    lateinit var presentation : Presentation
+    @BeforeEach
+    fun setUp () {
+        MockKAnnotations.init(this)
+        presentation = spyk<Presentation>()
+    }
+
+    @Test
+    fun `when a caret is selected` () {
+        every { actionEvent.getRequiredData(CommonDataKeys.EDITOR).caretModel } returns caret
+        every { caret.currentCaret.hasSelection() } returns true
+        every { actionEvent.presentation } returns presentation
+        val sut = ObjectCreateOnCaretSelectedAction()
+
+        sut.update(actionEvent)
+
+        assertTrue { actionEvent.presentation.isEnabledAndVisible }
+    }
+
+    @Test
+    fun `when selected file is java file` () {
+        val file = mockk<VirtualFile>()
+        every { actionEvent.getData(PlatformDataKeys.VIRTUAL_FILE) } returns file
+        every { file.toString() } returns "name.java"
+        every { caret.currentCaret.hasSelection() } returns true
+        every { actionEvent.presentation } returns presentation
+        val sut = ObjectCreateFileSeletedAction()
+
+        sut.update(actionEvent)
+
+        assertTrue { actionEvent.presentation.isEnabledAndVisible }
+        assertTrue { actionEvent.presentation.isEnabled }
+        assertTrue { actionEvent.presentation.isVisible }
+    }
+}
