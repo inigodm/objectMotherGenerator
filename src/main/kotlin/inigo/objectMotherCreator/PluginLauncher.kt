9d881eccb3b6f76b22a12923791108a36a21e936
inigodm <inigo.delgado@gmail.com>
2020-12-13 18:16:18 +0100

    Autodetect test source directory if exists and unique

--- src/main/kotlin/inigo/objectMotherCreator/PluginLauncher.kt
+++ src/main/kotlin/inigo/objectMotherCreator/PluginLauncher.kt
@@ -6,24 +6,46 @@ import com.intellij.openapi.fileChooser.FileChooser
 import com.intellij.openapi.fileChooser.FileChooserDescriptor
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ModuleRootManager
+import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiJavaFile
 import com.intellij.psi.PsiManager
+import org.jetbrains.jps.model.java.JavaSourceRootType
 import java.io.File
 
 class PluginLauncher {
 
     fun doObjectMotherCreation(e: AnActionEvent) {
-        FileChooser.chooseFile(buildFileChooserDescriptor(), e.project, e.getData(CommonDataKeys.VIRTUAL_FILE)) {
-            val dir = PsiManager.getInstance(e.project!!).findDirectory(it) ?: return@chooseFile
-            objectMotherCreation(e, dir)
+        var dirVirtualFile: VirtualFile? = findTestSourceDirectory(e)
+        if (dirVirtualFile == null){
+            return
         }
+        val dir = PsiManager.getInstance(e.project!!).findDirectory(dirVirtualFile!!)
+        objectMotherCreation(e, dir)
+    }
+
+    private fun findTestSourceDirectory(e: AnActionEvent): VirtualFile? {
+        val selectedFile = e.getData(CommonDataKeys.VIRTUAL_FILE)!!
+        val module = ProjectFileIndex.getInstance(e.project!!).getModuleForFile(selectedFile)!!
+        val moduleRootManager = ModuleRootManager.getInstance(module)
+        val testSources = moduleRootManager.getSourceRoots(JavaSourceRootType.TEST_SOURCE)
+        var dirVirtualFile: VirtualFile? = null
+        if (testSources.size == 1) {
+            dirVirtualFile = testSources[0]
+        }
+        if (dirVirtualFile == null) {
+            FileChooser.chooseFile(buildFileChooserDescriptor(), e.project, e.getData(CommonDataKeys.VIRTUAL_FILE)) {
+                dirVirtualFile = it
+            }
+        }
+        return dirVirtualFile
     }
 
     private fun buildFileChooserDescriptor(): FileChooserDescriptor {
-        var descriptor = FileChooserDescriptor(false, true, false, false, false, false)
+        val descriptor = FileChooserDescriptor(false, true, false, false, false, false)
         descriptor.title = "Choose project/module's test srcDir root in which ObjectMother's package will be created"
         descriptor.description =
             "The source directory for ObjectMother's package must be set. Package would be automatically calculated"
