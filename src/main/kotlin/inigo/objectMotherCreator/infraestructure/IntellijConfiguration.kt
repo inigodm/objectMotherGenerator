9f05ea04a95c2b3a0457363ae6fbf974866763e6
inigodm <inigo.delgado@gmail.com>
2023-02-14 19:32:29 +0100

    Fix page UI

--- src/main/kotlin/inigo/objectMotherCreator/infraestructure/IntellijConfiguration.kt
+++ src/main/kotlin/inigo/objectMotherCreator/infraestructure/IntellijConfiguration.kt
@@ -1,53 +1,54 @@
 package inigo.objectMotherCreator.infraestructure;
 
 import com.intellij.openapi.options.Configurable
+import org.jdesktop.swingx.VerticalLayout
 import java.awt.FlowLayout
 import javax.swing.*
 
 
 class IntellijConfiguration: Configurable {
     private val fakerTextField: JTextField = JTextField()
+    private val methodPrefixTextField: JTextField = JTextField()
 
     override fun createComponent(): JComponent {
         val main = JPanel()
-        val label = JLabel("Faker imported class")
-        main.layout = BoxLayout(main, BoxLayout.Y_AXIS)
-        val pack = JPanel(FlowLayout(FlowLayout.LEFT))
-        fakerTextField.text = IntellijPluginService.getAppInstance().state.state
-        pack.add(label)
-        pack.add(fakerTextField)
-        main.add(pack)
-        /*val border = JPanel(FlowLayout(FlowLayout.LEFT))
-                val main = JPanel()
-                main.setLayout(BoxLayout(main, BoxLayout.Y_AXIS))
-                border.add(main, BorderLayout.NORTH)
-                fakerTextField.text = IntellijPluginService.getAppInstance().state.state
-
-                var panel = JPanel(FlowLayout(FlowLayout.LEFT))
-                panel.add(fakerTextField)
-                val cleanHistoryBtn = JButton()
-                cleanHistoryBtn.setText("Clear History")
-                panel.add(cleanHistoryBtn)
-                main.add(panel)
-
-                panel = JPanel(FlowLayout(FlowLayout.LEFT))
-                panel.add(Label("Vamooooooos"))
-                main.add(panel)
-
-                panel = JPanel(FlowLayout(FlowLayout.LEFT))
-                panel.add(Label("SFTP settings"))
-                main.add(panel)*/
-
+        main.layout = VerticalLayout()
+        main.add(buildFakerClassnameComponent())
+        main.add(buildMethodPrefixComponent())
         reset()
         return main
     }
 
+    private fun buildFakerClassnameComponent(): JPanel {
+        return textFieldWithLabel(
+            "Faker imported class",
+            IntellijPluginService.getAppInstance().state.fakerClassname,
+            fakerTextField)
+    }
+
+    private fun buildMethodPrefixComponent(): JPanel {
+        return textFieldWithLabel(
+            "Prefix of builder functions",
+            IntellijPluginService.getAppInstance().state.methodsPrefix,
+            methodPrefixTextField)
+    }
+
+    private fun textFieldWithLabel(labelText : String, initialValue: String, textField: JTextField): JPanel {
+        val pack = JPanel(FlowLayout(FlowLayout.LEADING))
+        textField.text = initialValue
+        val label = JLabel(labelText)
+        pack.add(label)
+        pack.add(textField)
+        return pack
+    }
+
     override fun isModified(): Boolean {
-        return fakerTextField.text != IntellijPluginService.getAppInstance().state.state;
+        return fakerTextField.text != IntellijPluginService.getAppInstance().state.fakerClassname ||
+        methodPrefixTextField.text != IntellijPluginService.getAppInstance().state.methodsPrefix
     }
 
     override fun apply() {
-        IntellijPluginService.getAppInstance().loadState(PluginState(fakerTextField.text))
+        IntellijPluginService.getAppInstance().loadState(PluginState(fakerTextField.text, methodPrefixTextField.text))
     }
 
     override fun getDisplayName(): String {
